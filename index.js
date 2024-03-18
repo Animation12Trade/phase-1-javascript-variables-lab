d487249d88a54523c4e9c25146a9739ea50f480e
+ <omuhindaaaron@gmail.com>
2024-03-18 16:10:20 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'companyName';
+let companyCeo = 'mostProfitableNeighborhood';
+let Chelsea = 'companyName';
+mostProfitableNeighborhood = 'Chelsea';
+companyCeo = 'Susan Smith';
+
